1766790ecfb4f0d4e4ccb4f62230ae8e089a3139
rayvzor <zadrot-vzor2@mail.ru>
2017-07-01 12:59:46 +0300

    изменения в меню

--- index/menu.php
+++ index/menu.php
@@ -1,6 +1,6 @@
 <?php 
 if($_SESSION['user'] === SES_ADMIN or $_SESSION['user'] === SES_PRODAVEC){
-  $margin = '20';
+  $margin = '120';
 }else{
   $margin = '40';
 }
@@ -11,22 +11,10 @@ if($_SESSION['user'] === SES_ADMIN or $_SESSION['user'] === SES_PRODAVEC){
 			  <a href="<?php echo $site; ?>">Главная</a><br>
 		    </li>
 		    <li>
-			  <a href="<?php echo $site; ?>sklad">Склад</a><br>
+			  <a href="<?php echo $site; ?>sklad">Заказы</a><br>
 		    </li>
 			<li>
-			  <a href="<?php echo $site; ?>otgruz">Отгрузка</a><br>
-		    </li>
-			<li>
-			  <a href="<?php echo $site; ?>otgruzki">Отгрузки</a><br>
-		    </li>
-			<li>
-			  <a href="<?php echo $site; ?>balans?histori_date=<?php echo $date; ?>">Баланс</a><br>
-		    </li>
-			<li>
-			  <a href="<?php echo $site; ?>istoriya?histori_date=<?php echo $date; ?>">История</a><br>
-		    </li>
-			<li>
-			  <a href="<?php echo $site; ?>uchet?histori_date=<?php echo $date; ?>">Учет</a><br>
+			  <a href="<?php echo $site; ?>otgruz">Операторы</a><br>
 		    </li>
 			<li>
 			</li>
